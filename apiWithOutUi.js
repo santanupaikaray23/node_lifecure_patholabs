d1b4abea7809a13c0b494d69eef700089e771246
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2022-07-24 07:49:50 +0530

    add all changes

--- apiWithOutUi.js
+++ apiWithOutUi.js
@@ -5,6 +5,7 @@ const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
 const bodyParser = require('body-parser')
 const cors = require('cors');
+const res = require('express/lib/response');
 // const res = require('express/lib/response');
 app.use(cors())
 const mongourl="mongodb://localhost:27017";
@@ -16,6 +17,11 @@ let col_name3="dashboard3"
 let col_name4="dashboard4"
 let col_name5="dashboard5"
 let col_name6="dashboard6"
+let col_name7="dashboard7"
+let col_name8="dashboard8"
+let col_name9="dashboard9"
+let col_name10="dashboard10"
+let col_name11="dashboard11"
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
@@ -193,6 +199,134 @@ app.get('/placeBooking/:id',(req,res) => {
         res.send(result)
     })
 
+})
+
+// app.get('/paymentBooking',(req,res)=>{
+//     db.collection(col_name5).find().toArray((err,result)=>{
+//         if(err) throw err;
+//         res.send(result)
+//     })
+    
+//     })
+//       //Insert
+//       app.post('/addpaymentBooking',(req,res)=>{
+//         console.log(req.body)
+//         db.collection(col_name5).insert(req.body,(err,result) => {
+//             if(err) throw err;
+//             res.send('Data Added')
+//         })
+//     })
+//Read
+app.get('/Designation',(req,res)=>{
+    db.collection(col_name7).find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+//Insert
+app.post('/addDesignation',(req,res)=>{
+    console.log(req.body)
+    db.collection(col_name7).insert(req.body,(err,result)=>{
+        if(err) throw err;
+        res.send('Data Added')
+
+    })
+})
+//Read
+app.get('/Gender',(req,res)=>{
+    var query = {}
+    if(req.query.service){
+        query={service:req.query.service,isActive:true}
+    }else{
+        query = {isActive:true}
+
+    }
+    
+    db.collection(col_name8).find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+//GenderDetails
+app.get('/Gender/:id',(req,res)=>{
+    var id = mongo.ObjectId(req.params.id)
+    
+    db.collection(col_name8).find({_id:id}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+app.post('/addGender',(req,res)=>{
+    console.log(req.body)
+    db.collection(col_name8).insert(req.body,(err,result)=>{
+        if(err) throw err;
+        res.send('Data Added')
+
+    })
+})
+//update
+// app.put('/updateGender',(req,res)=>{
+//     db.collection(col_name8).updateOne(
+//         {_id:mongo.ObjectId(res.body._id)},
+//         {
+//             $set:{
+                
+                    
+//                     service_name:req.body.service_name,
+//                     service:req.body.service,
+//                     isActive:true
+                
+//             }
+//         },(err,result)=>{
+//             if(err) throw err;
+//             res.send('Data Updated')
+//         }
+//     )
+// })
+app.get('/Testname',(req,res)=>{
+    db.collection(col_name9).find().toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+app.post('/addTestname',(req,res)=>{
+    console.log(req.body)
+    db.collection(col_name9).insert(req.body,(err,result)=>{
+        if(err) throw err;
+        res.send('Data Added')
+
+    })
+})
+app.get('/Price',(req,res)=>{
+    var query = {}
+    if(req.query.service){
+        query={service:req.query.service,isActive:true}
+    }else{
+        query = {isActive:true}
+
+    }
+    
+    db.collection(col_name10).find(query).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+//GenderDetails
+app.get('/Price/:id',(req,res)=>{
+    var id = mongo.ObjectId(req.params.id)
+    
+    db.collection(col_name10).find({_id:id}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
+app.post('/addPrice',(req,res)=>{
+    console.log(req.body)
+    db.collection(col_name10).insert(req.body,(err,result)=>{
+        if(err) throw err;
+        res.send('Data Added')
+
+    })
 })
 
 
